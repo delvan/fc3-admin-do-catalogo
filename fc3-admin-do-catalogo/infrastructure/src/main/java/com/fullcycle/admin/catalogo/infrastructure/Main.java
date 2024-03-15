b21f0646863e8416a152914d462f1dc47cdf875b
root <root@DESKTOP-RMTNND0>
2024-03-15 13:30:55 -0300

    Configurando o datasource e Customizando o hibernate e o hikari para alta performance

--- fc3-admin-do-catalogo/infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ fc3-admin-do-catalogo/infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Word");
         //System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "production");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
